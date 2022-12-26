ff9aef308b80fa81d168d33289b68379c580baa3
Sidhant Shahi <starindustries05@gmail.com>
2022-12-27 03:28:56 +0530

    listening to mail1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.post('/mail', (req, res) => {
-    console.log(req.body)
+    console.log(req)
     res.status(200).send('OK')
 })
 app.listen(process.env.PORT || 3000)
