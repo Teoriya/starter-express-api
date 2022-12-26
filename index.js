dabf75e814651d0ef205a3d0899b803ad4b86876
Sidhant Shahi <starindustries05@gmail.com>
2022-12-27 04:05:05 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json({type: 'application/gzip'}))
 app.post('/mail', (req, res) => {
     console.log(req)
     res.status(200).send('OK')
